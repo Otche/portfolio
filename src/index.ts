05f07c0657508a573cdec3944be949971dea7372
Madjid <djerada.madjid@gmail.com>
2024-09-11 20:07:00 +0200

    init dynmique templating of pages with ejs

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,65 @@
 import express from 'express'
 import path from 'path'
+import ejs from 'ejs'
 
 const PORT = process.env.PORT || 3000
 const app = express()
+const staticFile = express()
 
-app.get('*', (req, res) => {
+app.use('/public', staticFile)
+
+const templatingVars = (url: string) => {
+  return {
+    currentUrl: url,
+    pages: [
+      {
+        url: './home.html',
+        label: 'Home',
+        className: url === '/home.html' ? 'active' : '',
+      },
+      {
+        url: './services.html',
+        label: 'Serive',
+        className: url === '/services.html' ? 'active' : '',
+      },
+      {
+        url: './career.html',
+        label: 'Carrière',
+        className: url === '/career.html' ? 'active' : '',
+      },
+      {
+        url: './contact.html',
+        label: 'Contact',
+        className: url === '/contact.html' ? 'active' : '',
+      },
+    ],
+  }
+}
+
+staticFile.get('*', (req, res) => {
   const pagePath =
     req.url === '/normalize.css'
       ? 'node_modules/normalize.css/normalize.css'
       : `public/${req.url}`
   res.status(200).sendFile(path.resolve(pagePath))
+})
+
+app.get('/*.html', (req, res) => {
+  const pageFileToRenderPath = path.resolve(
+    `templates${req.url.replace('html', 'ejs')}`
+  )
+
+  ejs.renderFile(
+    pageFileToRenderPath,
+    templatingVars(req.url),
+    {
+      root: [path.resolve('templates')],
+    },
+    (err, htmlStringPage) => {
+      if (err) return res.sendStatus(404)
+      res.status(200).send(htmlStringPage)
+    }
+  )
 })
 
 app.listen(PORT, () => {
